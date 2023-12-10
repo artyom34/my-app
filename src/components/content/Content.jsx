09995c8d17868070d2f6d431fee69aa95cd6b708
Artyom Cheremisin <art.cheremisin@gmail.com>
2023-12-10 12:19:56 +0200

    added a dialogs to the component

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import classes from "./content.module.css";
+import Dialogs from "./dialogs/Dialogs";
 import Profile from "./profile/Profile";
 
 const Content = () => {
   return (
     <div className={classes.content}>
       <Profile />
+      <Dialogs />
     </div>
   );
 };
